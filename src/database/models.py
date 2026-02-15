acb0c3112a0baad91d197308ea6664341c0afe52
alt047 <alet4343@gmail.com>
2026-02-15 08:44:49 +0100

    Create models.py

---
+++ src/database/models.py
@@ -0,0 +1,27 @@
+from sqlalchemy import Column, DateTime, Float, ForeignKey, Integer, String
+from sqlalchemy.orm import relationship, declarative_base
+
+Base = declarative_base()
+
+class Assets(Base):
+    __tablename__ = 'assets'
+    
+    id = Column(Integer, primary_key=True)
+    name = Column(String, nullable=False)
+    ticker = Column(String, unique=True, nullable=False)
+    prices = relationship("DailyPrice", back_populates="asset")
+    
+class DailyPrice(Base):
+    __tablename__ = 'daily_prices'
+    
+    id = Column(Integer, primary_key=True)
+    asset_id = Column(Integer, ForeignKey('assets.id'))
+    
+    date = Column(DateTime, nullable=False)
+    open = Column(Float)
+    high = Column(Float)
+    low = Column(Float)
+    close = Column(Float)
+    volume = Column(Integer)
+    
+    asset = relationship("Assets", back_populates="prices")
