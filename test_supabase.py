8d5fb72c1029eaf050c7117bf66c71afa739ead5
alt047 <alet4343@gmail.com>
2026-02-16 15:00:34 +0100

    Update test_supabase.py

--- test_supabase.py
+++ test_supabase.py
@@ -13,8 +13,8 @@ client = TiingoClient(config)
 historical_data = client.get_ticker_price(
     "AAPL",
     fmt='json',
-    startDate='2023-01-01',
-    endDate='2023-12-31',
+    startDate='1981-01-01',
+    endDate='1981-12-31',
     frequency='daily'
 )
 
